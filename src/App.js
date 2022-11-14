c76b771dcaf2d350fcf072e0c96f4416368e6c8a
FarhanBinShafiq <farhan.shafiq@northsouth.edu>
2022-11-14 15:16:46 +0600

    Done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Doctor from "./components/Doctors/Doctor";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 
 
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="doctor" element={<Doctor />}></Route>
           <Route path="blogs" element={<Blogs />}></Route>
           <Route path="contact" element={<Contact />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
         <Footer></Footer>
       </BrowserRouter>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css'
+
+const NotFound = () => {
+    return (
+        <div className='not-found'>
+            <h1>404-Not Found</h1>
+            <h2>This url is not correct</h2>
+        </div>
+    );
+};
+
+export default NotFound;
